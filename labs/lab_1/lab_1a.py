225b02d87c066789831dc114583cfdff4e54abaa
n0tElectro <ZHANGXUANQI0223@GMAIL.COM>
2026-03-29 21:49:43 -0700

    Added description

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -10,7 +10,7 @@ def main():
 
     name = "Dylan Zhang" # TODO: Insert your name between the double quotes
 
-    print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! I am Dylan Zhang and I attend school in the state of California. I am an 8th grader chasing my dreams and I would love to learn more about computer science and electronics related subjects. In my free time I would make small projects and learn new things on my Arduino and sometimes make websites or learn new programming languages on freeCodeCamp. I hope I can learn new skills in this course and thanks everyone!")
 
 if __name__ == "__main__":
     main()
